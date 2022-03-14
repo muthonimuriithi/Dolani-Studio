2b290b1cca4f999231e96d4a6d0bd58efd42bbbb
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-03-14 16:00:16 +0300

    added form validation

--- script.js
+++ script.js
@@ -3,21 +3,65 @@ $(document).ready(function(){
     $("#design").click(function(){
         $("#d-showing").toggle();
         $(".h-image").toggle();
-      });
+    });
     
-      $("#development").click(function(){
+    $("#development").click(function(){
         $("#dp-showing").toggle();
         $(".hd-image").toggle();
-      })
+    })
     
-      $("#productManagement").click(function(){
+    $("#productManagement").click(function(){
         $("#pm-showing").toggle();
         $(".h-pmanagement").toggle();
-      })
-      $(".Hover").hover(function(){
-        $(this).animate({opacity:'1'});
-      },
-      function(){
-        $(this).animate({opacity:'0'});
-      })
-})
+    })
+
+    $(`#first`).submit(function(){
+        alert(`Hi` + $("#name").val()+ `` + `We have received your message. Thank you for reaching out to us`);
+    });
+    
+    $("#pic1").mouseover(function(){
+        $(".bar").show();
+    }).mouseout(function(){
+        $(".bar").hide();
+    });
+    $("#pic2").mouseover(function(){
+        $(".bar2").show();
+    }).mouseout(function(){
+        $(".bar2").hide();
+    });
+    $("#pic3").mouseover(function(){
+        $(".bar3").show();
+    }).mouseout(function(){
+        $(".bar3").hide();
+    });
+    $("#pic4").mouseover(function(){
+        $(".bar4").show();
+    }).mouseout(function(){
+        $(".bar4").hide();
+    });
+    $("#pic5").mouseover(function(){
+        $(".bar5").show();
+    }).mouseout(function(){
+        $(".bar5").hide();
+    });
+    $("#pic6").mouseover(function(){
+        $(".bar6").show();
+    }).mouseout(function(){
+        $(".bar6").hide();
+    });
+    $("#pic7").mouseover(function(){
+        $(".bar7").show();
+    }).mouseout(function(){
+        $(".bar7").hide();
+    });
+    $("#pic8").mouseover(function(){
+        $(".bar8").show();
+    }).mouseout(function(){
+        $(".bar8").hide();
+    });
+
+});
+
+
+
+
